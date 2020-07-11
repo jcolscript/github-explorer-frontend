e512fd207bba745d4d877a5cfa263f47ebe606bf
José Carlos <jcolscript@gmail.com>
2020-07-11 03:09:07 -0300

    conectando na API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,77 +1,65 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import logo from '../../assets/images/logo.svg';
 
 import { Title, Form, Users } from './styles';
 
-const Dashboard: React.FC = () => (
-  <>
-    <img src={logo} alt="GitHub Explorer" />
-    <Title>Explore repositórios no Github.</Title>
-    <Form>
-      <input type="text" placeholder="Digite aqui o username" />
-      <button type="submit">Pesquisar</button>
-    </Form>
-    <Users>
-      <a href="#">
-        <img
-          src="https://avatars2.githubusercontent.com/u/30646629?s=460&u=18b54427adec2bc4d9e7b06f088fc6881d4fdb9a&v=4"
-          alt=""
+interface IUsers {
+  login: string;
+  id: number;
+  avatar_url: string;
+  url: string;
+  name: string;
+  bio: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+const Dashboard: React.FC = () => {
+  const [users, setUsers] = useState<IUsers[]>([]);
+  const [newUser, setNewUser] = useState('');
+
+  async function handleAddUser(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const { data } = await api.get<IUsers>(`users/${newUser}`);
+
+    setUsers([...users, data]);
+    setNewUser('');
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GitHub Explorer" />
+      <Title>Explore repositórios no Github.</Title>
+      <Form onSubmit={handleAddUser}>
+        <input
+          type="text"
+          placeholder="Digite aqui o username"
+          onChange={e => setNewUser(e.target.value)}
+          value={newUser}
         />
-        <div>
-          <strong>jcolscript</strong>
-          <p>
-            I believe that technologies come and go, so I'm really passionate
-            about learning new frameworks, languages ​​and paradigms.
-          </p>
-        </div>
-        <FiChevronRight size={20} />
-      </a>
-      <a href="#">
-        <img
-          src="https://avatars2.githubusercontent.com/u/30646629?s=460&u=18b54427adec2bc4d9e7b06f088fc6881d4fdb9a&v=4"
-          alt=""
-        />
-        <div>
-          <strong>jcolscript</strong>
-          <p>
-            I believe that technologies come and go, so I'm really passionate
-            about learning new frameworks, languages ​​and paradigms.
-          </p>
-        </div>
-        <FiChevronRight size={20} />
-      </a>
-      <a href="#">
-        <img
-          src="https://avatars2.githubusercontent.com/u/30646629?s=460&u=18b54427adec2bc4d9e7b06f088fc6881d4fdb9a&v=4"
-          alt=""
-        />
-        <div>
-          <strong>jcolscript</strong>
-          <p>
-            I believe that technologies come and go, so I'm really passionate
-            about learning new frameworks, languages ​​and paradigms.
-          </p>
-        </div>
-        <FiChevronRight size={20} />
-      </a>
-      <a href="#">
-        <img
-          src="https://avatars2.githubusercontent.com/u/30646629?s=460&u=18b54427adec2bc4d9e7b06f088fc6881d4fdb9a&v=4"
-          alt=""
-        />
-        <div>
-          <strong>jcolscript</strong>
-          <p>
-            I believe that technologies come and go, so I'm really passionate
-            about learning new frameworks, languages ​​and paradigms.
-          </p>
-        </div>
-        <FiChevronRight size={20} />
-      </a>
-    </Users>
-  </>
-);
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Users>
+        {users.map(user => (
+          <a href="teste" key={user.id}>
+            <img src={user.avatar_url} alt="" />
+            <div>
+              <strong>{user.login}</strong>
+              <p>{user.bio}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </Users>
+    </>
+  );
+};
 
 export default Dashboard;
